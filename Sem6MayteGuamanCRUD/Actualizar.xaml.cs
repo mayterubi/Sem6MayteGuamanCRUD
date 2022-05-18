68912b0fd60d5614e5105499b0662a10ed8fb8c9
mayterubiela <85627608+mayterubiela@users.noreply.github.com>
2022-05-18 15:45:09 -0500

    Agregar archivos de proyecto.

---
+++ Sem6MayteGuamanCRUD/Actualizar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sem6MayteGuamanCRUD
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Actualizar : ContentPage
+    {
+        private const string Url = "http://192.168.100.173/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Datos> _post;
+
+        public Actualizar()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<Datos> posts = JsonConvert.DeserializeObject<List<Datos>>(content);
+            _post = new ObservableCollection<Datos>(posts);
+
+            MyListView.ItemsSource = _post;
+        }
+    }
+}
---
+++ Sem6MayteGuamanCRUD/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sem6MayteGuamanCRUD
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues("http://192.168.100.173/moviles/post.php", "POST", parametros);
+
+                DisplayAlert("Mensaje de alerta", "Ingreso Correcto", "OK");
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Mensaje de alerta", "Algo Salió Mal", "OK");
+            }
+        }
+
+        private void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
